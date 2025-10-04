bc2c0c83c8c2b71d1ad236db41f9e6b8c5239f00
openkey505 <aliopenkey2010@gmail.com>
2025-10-04 19:46:58 +0330

    add Complate H and F

--- Frontend/assets/js/script.js
+++ Frontend/assets/js/script.js
@@ -2,9 +2,32 @@ let searchIcon = document.getElementById("searchIcon");
 let searchBox = document.getElementById("searchBox");
 let navList = document.getElementById("navList");
 let btnLogReg = document.getElementById("btnLogReg");
+let overlay = document.querySelector(".overlay");
 
+let dropDown = document.getElementById("dropDown");
+let dropDownSec = document.getElementById("dropDownSec");
+
+// SearchBox Section
 searchIcon.addEventListener("click", function() {
     searchBox.style.display = "block";
+    overlay.style.display = "block";
     navList.style.opacity = "0";
     btnLogReg.style.opacity = "0";
+})
+
+overlay.addEventListener("click", function() {
+    searchBox.style.display = "none";
+    overlay.style.display = "none";
+    navList.style.opacity = "1";
+    btnLogReg.style.opacity = "1";
+})
+
+// DropDown Rezerve Section
+dropDown.addEventListener("click", function() {
+    if(dropDownSec.style.display == "none") {
+        
+        dropDownSec.style.display = "block";
+    } else {
+        dropDownSec.style.display = "none";
+    }
 })
